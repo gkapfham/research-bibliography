bb9e1185aaaa4c2a56d43f1fbae039e5721905bb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-11-29 21:02:05 -0500

    Updating the referencecounter; basic working version for analysis.

--- referencecounter.sh
+++ referencecounter.sh
@@ -11,15 +11,23 @@ echo ""
 ag --nocolor --nonumbers @inproceedings bibtex/bibliography_kapfhammer.bib
 
 echo ""
+printf "%s\n" "${red}I found these @incollection entries! ${end}"
+echo ""
+ag --nocolor --nonumbers @incollection bibtex/bibliography_kapfhammer.bib
+
+echo ""
 printf "%s\n" "${red}I found these @article entries! ${end}"
 echo ""
 ag --nocolor --nonumbers @article bibtex/bibliography_kapfhammer.bib
-
 
 # display the summary counts for the different categories
 echo ""
 printf "%s " "${red}Count of @inproceedings:${end}"
 ag @inproceedings bibtex/bibliography_kapfhammer.bib | wc -l
 
+printf "%s   " "${red}Count of @incollection:${end}"
+ag @incollection bibtex/bibliography_kapfhammer.bib | wc -l
+
 printf "%s        " "${red}Count of @article:${end}"
 ag @article bibtex/bibliography_kapfhammer.bib | wc -l
+echo ""
